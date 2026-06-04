400f2fa61738ab13675095e9508b6265c857731f
Gouda-cyber <animerlmohamed@gmail.com>
2026-06-04 15:06:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const birdImg = new Image();
-birdImg.src = "images/samra.png";
+birdImg.src = "samra.png";
 
 const bgImg = new Image();
 bgImg.src = "images/bg.png";
